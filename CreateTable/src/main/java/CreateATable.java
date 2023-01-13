31cd55088c5810ab972bcdda35ee5903da456a98
RAN-co <artyrebrina@gmail.com>
2023-01-13 01:16:33 +0000

    Saved progress on CreateTable 2023-01-13 01:16:33 +0000

--- CreateTable/src/main/java/CreateATable.java
+++ CreateTable/src/main/java/CreateATable.java
@@ -26,7 +26,7 @@ import java.sql.Statement;
  * The syntax for Creating a table is as follows:
  * CREATE TABLE table_name(
  *      variable_name1 datatype constraint,
- *      variable_name2 datatype constraint
+ *      variable_name2 datatype 
  * );
  *
  * Note: there are two terms in the above syntax that we need to understand:
