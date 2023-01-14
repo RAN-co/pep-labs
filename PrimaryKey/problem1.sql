1685f32c6b78cffe75d896d75f33733b1769f23d
RAN-co <artyrebrina@gmail.com>
2023-01-14 01:54:46 +0000

    Saved progress on PrimaryKey 2023-01-14 01:54:46 +0000

--- PrimaryKey/problem1.sql
+++ PrimaryKey/problem1.sql
@@ -1,9 +1,5 @@
 CREATE TABLE song (
- *          id SERIAL PRIMARY KEY,
- *          title varchar(100),
- *          artist varchar(100)
- *      );
-INSERT song (id) VALUES 
-    (1,2,3),
-    ('Let it be','Hotel California','Kashmir'),
-    ('Beatles','Eagles','Led Zeppelin');
+           id SERIAL PRIMARY KEY,
+           title varchar(100),
+          artist varchar(100)
+       );
