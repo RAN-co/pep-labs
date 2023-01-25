b48bf9a106e7a305a548d9a185fca25d33519acb
RAN-co <artyrebrina@gmail.com>
2023-01-25 02:55:36 +0000

    Saved progress on InnerJoin 2023-01-25 02:55:36 +0000

--- InnerJoin/problem1.sql
+++ InnerJoin/problem1.sql
@@ -1,1 +1,1 @@
-SELECT FROM class INNER JOIN student ON class.class_title = student.class_title WHERE class.class_title = 'Physics' AND 'Math';
+SELECT class.teacher_name, student.id, student.student_name FROM class INNER JOIN student ON class.class_title = student.class_title WHERE class.teacher_name = 'Ms. Lovelace';
