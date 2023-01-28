6f27572329bb26bf2358aea1444928948e88c44a
RAN-co <artyrebrina@gmail.com>
2023-01-28 01:44:49 +0000

    Saved progress on ContainsDuplicates 2023-01-28 01:44:49 +0000

--- Search2dArray/src/main/java/Search2dArray.java
+++ Search2dArray/src/main/java/Search2dArray.java
@@ -1,21 +1,25 @@
 public class Search2dArray {
     /**
      * Receive a 2d array of ints. (That's an array of arrays!)
-     * Your task is to search the array for a matching value, return true if found false otherwise.
-     * You'll need to use a for loop within a for loop - each for loop handles searching in a particular dimension.
+     * Your task is to search the array for a matching value, return true if found
+     * false otherwise.
+     * You'll need to use a for loop within a for loop - each for loop handles
+     * searching in a particular dimension.
      * I recommend reasoning through this problem on pencil and paper.
      *
-     * @param nums a two dimensional array - meaning, an array of arrays.
+     * @param nums   a two dimensional array - meaning, an array of arrays.
      * @param target a value that may be contained within nums.
      * @return true if nums contains target, and false otherwise.
      */
-    public boolean search(int[][] nums, int target){
-        for(int i = 0; i < nums.length; i++){
-            for(int j = 0; j < nums.length; j++){
-                if(nums[i][j] == target){
+
+    public boolean search(int[][] nums, int target) {
+        int i;
+        int j;
+        for (i = 0; i < nums.length; i++) {
+            for (j = 0; j < nums.length; j++)
+
+                if (nums[i][j] !=0 && nums[i][j] == target)
                     return true;
-                }
-            }
         }
         return false;
     }
