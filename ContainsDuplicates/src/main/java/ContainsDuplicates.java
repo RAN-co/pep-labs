72d0eefa09d984ad194d910a617eb44881f95aaa
RAN-co <artyrebrina@gmail.com>
2023-01-28 02:04:35 +0000

    Saved progress on ContainsDuplicates 2023-01-28 02:04:35 +0000

--- ContainsDuplicates/src/main/java/ContainsDuplicates.java
+++ ContainsDuplicates/src/main/java/ContainsDuplicates.java
@@ -1,4 +1,5 @@
-
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContainsDuplicates {
     /**
@@ -8,6 +9,13 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+       Set<Integer> trickster = new HashSet<Integer>();
+        for(int arrUnit: nums){
+            if(trickster.contains(arrUnit) == false){
+                return true;
+            }
+            trickster.add(arrUnit);
+        }
         return false;
     }
 }
