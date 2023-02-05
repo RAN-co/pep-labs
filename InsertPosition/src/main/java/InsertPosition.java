bf99240a8b16037990d3e8e84d7e9af58b3b51f3
RAN-co <artyrebrina@gmail.com>
2023-02-05 01:58:46 +0000

    Saved progress on ParentObjectClassMethods 2023-02-05 01:58:46 +0000

--- InsertPosition/src/main/java/InsertPosition.java
+++ InsertPosition/src/main/java/InsertPosition.java
@@ -15,21 +15,6 @@ public class InsertPosition {
      * @return return the index that a number should be inserted into a sorted array.
      */
     public int searchInsert(int[] nums, int target) {
-        int left = 0;
-    int right = nums.length - 1;
-    
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        
-        if (nums[mid] == target) {
-            return mid;
-        } else if (nums[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-    
-    return left;
+        return -1;
     }
 }
